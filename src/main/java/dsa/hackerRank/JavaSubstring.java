d6e4e35b10b8f8e93f014b83ff4ed2bddec47bcc
Mehemmed <yerivar0@gmail.com>
2026-05-07 16:44:05 +0400

    tasks

---
+++ src/main/java/dsa/hackerRank/JavaSubstring.java
@@ -0,0 +1,14 @@
+package dsa.hackerRank;
+
+import java.util.Scanner;
+
+public class JavaSubstring {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        String ss=S.substring(start,end);
+        System.out.println(ss);
+    }
+}
